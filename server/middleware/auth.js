7b7ba209978e404849e3be9b20bdf7616dc77198
Tawhid Islam <tawhidahmed.me@gmail.com>
2025-04-27 17:49:35 +1000

    fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import User from path.join(__dirname, '../models/User.js');
+import User from '../models/User.js';
 
 const authMiddleware = async (req, res, next) => {
   try {
